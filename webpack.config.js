629471d8403bd95d41e1e28e381d2adb85edb815
Zheyang Song <jerome_soung@hotmail.com>
2023-06-06 08:14:28 -0700

    fix vulnerability

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,15 @@ module.exports = {
   },
   resolve: {
     // Add '.ts' and '.tsx' as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".ts", ".tsx", ".js", "..."]
   },
   module: {
     rules: [
       // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
       {
         test: /\.tsx?$/,
-        use: "ts-loader"
+        exclude: ["/node_modules/"],
+        loader: "ts-loader"
       }
     ]
   }
